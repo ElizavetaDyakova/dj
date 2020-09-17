d3d3f1e21cd527fe562222d44faf297296682ee9
Elizaveta <63105777+ElizavetaDyakova@users.noreply.github.com>
2020-09-17 15:51:05 +0400

    Add files via upload

--- advito/models.py
+++ advito/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+def ava_path(instance, filename):
+    return 'user_{0}/avas/{1}'.format(instance.user.id, filename)
+
+
+def add_path(instance, filename):
+    return 'user_{0}/posts/{1}'.format(instance.author.id, filename)
+
+
+class Profile(models.Model):
+    '''
+    Модель пользователя
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+    birth_date = models.DateField('Date of birth', null=True, blank=True)
+    about = models.TextField('About', max_length=500, blank=True)
+    ava = models.ImageField(upload_to=ava_path, default=None)
+    phone_number = models.CharField(max_length=12, default=None)
+
+    def __str__(self):
+        return str(self.user.username)
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, verbose_name='название')
+
+
+class Add(models.Model):
+    '''
+    Объявление
+    '''
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    header = models.TextField(max_length=100)
+    description = models.TextField(max_length=1000, blank=True)
+    image = models.ImageField(upload_to=add_path)
+    favourites = models.ManyToManyField(User, related_name='users_like_it', blank=True)
+    date_pub = models.DateField(default=timezone.now)
+    date_edit = models.DateField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+
+    def __str__(self):
+        return 'Author {} date {}'.format(self.author.username, self.date_pub)
+
--- advito/urls.py
+++ advito/urls.py
@@ -4,6 +4,12 @@ from django.urls import path
 
 
 urlpatterns = [
-    url(r'^$', views.index_view, name='index'),
-    path('num', views.num),
+    url(r'^$', views.index, name='index'),
+    path('all/', views.all, name='all'),
+    path('post/create/', views.post_create, name='post_create'),
+    path('post/<int:add_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:add_id>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:add_id>/delete/', views.post_delete, name='post_delete'),
+    path('post/<int:add_id>/like/', views.like_post, name='like_post'),
+
 ]
